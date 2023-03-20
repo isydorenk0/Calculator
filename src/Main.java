2759e046dff9706684c0127affe3be0a6dfe6bbf
Taras <thezulkin@gmail.com>
2023-03-20 19:49:19 +0100

    Input/Output

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        int operant1, operant2, action, result;
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Ведіть перше число");
+        operant1 = scanner.nextInt();
+        System.out.println("Ведіть друге число число");
+        operant2 = scanner.nextInt();
+        System.out.println("Ведіть знак операції \n1 - додати \n2 - відняти \n3 - помножити \n4 - поділити");
+        action = scanner.nextInt();
 
+        System.out.println("Результат " + result);
     }
 }
