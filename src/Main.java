62fc920c71d6a4051bc17ca9a0bc32c8f4e44537
Ivanduik <ivictorv1982@gmail.com>
2023-03-20 20:50:12 +0200

    add function fAdd fsub fMul fDiv

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,30 @@
 import java.util.Scanner;
 
 public class Main {
+    static int fAdd(int a, int b) {
+
+        return a + b;
+    }
+
+    static int fSub(int a, int b) {
+
+        return a - b;
+    }
+
+    static int fMul(int a, int b) {
+
+        return a * b;
+    }
+
+    static float fDiv(int a, int b) {
+        if (b == 0) {
+            System.out.println("Divide by zero !!!");
+            return 0;
+        }
+        return a / b;
+    }
+
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
